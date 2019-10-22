af4a93a883698df5f20ef98407c4faaf6a47abca
tarrinros <tarindis@gmail.com>
2019-10-23 00:18:32 +0300

    Add CAr component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Car from './Car/Car'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello world!</h1>
+      <Car />
     </div>
   );
 }
---
+++ src/Car/Car.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Car() {
+  return (
+    <h2>This is Car component.</h2>
+  )
+}
+
+export default Car
